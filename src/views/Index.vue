d936457689bcfe499dc6914e0e9390ddad044890
akari nakamura <akarinakamura0305@gmail.com>
2020-08-31 20:52:15 +0900

    todo-itemを追加

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <h1>あああ</h1>
+    <todo-item />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+import TodoItem from "@/components/TodoItem.vue";
+
+export default defineComponent({
+  components: {
+    TodoItem
+  }
+});
+</script>
