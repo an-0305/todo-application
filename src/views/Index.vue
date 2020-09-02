514becc36deb88b04cf1043dfe475e6792b8530d
akari nakamura <akarinakamura0305@gmail.com>
2020-09-02 21:27:51 +0900

    ステータス別のデータを取得

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="list-wrapper">
-    <todo-list :title="title.todo" />
-    <todo-list :title="title.progress" />
-    <todo-list :title="title.done" />
+    <todo-list :title="title.todo" :items="todoItem" />
+    <todo-list :title="title.progress" :items="progressItem" />
+    <todo-list :title="title.done" :items="doneItem" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "@vue/composition-api";
+import { defineComponent, reactive, computed } from "@vue/composition-api";
 import TodoList from "@/components/TodoList.vue";
 
 export default defineComponent({
@@ -20,8 +20,29 @@ export default defineComponent({
       progress: "進行中",
       done: "完了"
     });
+    const content = reactive({
+      items: [
+        { title: "掃除をする", text: "今日中に掃除をする", status: 1 },
+        { title: "皿洗い", text: "皿を洗う", status: 1 },
+        { title: "買い物", text: "明日の朝ごはんを買いに行く", status: 2 },
+        { title: "洗濯", text: "午前中に洗濯機を回す", status: 3 }
+      ]
+    });
+    const todoItem = computed(() =>
+      content.items.filter(item => item.status === 1)
+    );
+    const progressItem = computed(() =>
+      content.items.filter(item => item.status === 2)
+    );
+    const doneItem = computed(() =>
+      content.items.filter(item => item.status === 3)
+    );
     return {
-      title
+      title,
+      content,
+      todoItem,
+      progressItem,
+      doneItem
     };
   }
 });
